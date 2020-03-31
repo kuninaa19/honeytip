1869c230c28664b2223751f6cda322f39986e674
kuninaa19 <minchan0527@naver.com>
2020-03-31 21:24:55 +0900

    (updated) web.php  PostsController를 미들웨어 CORS에 하나로 묶었음

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -7,7 +7,6 @@ Route::get('/', 'exampleController@index');
 // Ajax 연습페이지
 Route::get('/example', 'exampleController@example');
 Route::post('/idstore', 'exampleController@store');
-Route::get('/page', 'exampleController@page');
 
 // 관리자 로그인
 Route::middleware(['cors'])->group(function(){
@@ -17,14 +16,20 @@ Route::middleware(['cors'])->group(function(){
     Route::post('login','AdminController@login');
 });
 
-//글 관련 Route
-Route::resource('posts', 'PostsController',['except' => ['index','create']])->middleware('cors');
 
-//글작성 이미지저장
-Route::post('posts/image', 'PostsController@imageStore')->middleware('cors');
+Route::middleware(['cors'])->group(function(){
+    //글 관련 Route
+    Route::resource('posts', 'PostsController',['except' => ['index','create']]);
 
-//글 상세 내용페이지
-Route::get('posts/{category}/{num}', 'PostsController@content')->middleware('cors');
+    //글작성 이미지저장
+    Route::post('posts/image', 'PostsController@imageStore');
+
+    //카테고리별 글리스트
+    Route::get('posts/{category}/{num}', 'PostsController@category_list');
+
+    //글 조회수 +1 증가(작업 아직안함)
+    Route::get('posts/{num}', 'PostsController@viewUp');
+});
 
 // 댓글 관련 Route
 Route::resource('reply', 'ReplysController')->middleware('cors');
