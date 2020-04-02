2f6655e2df734cf8de10b0d088bc6139950e0448
kuninaa19 <minchan0527@naver.com>
2020-04-02 10:32:01 +0900

    (added) VerifyCsrfToken.php 댓글관련 url csrf 허용

--- blog/app/Http/Middleware/VerifyCsrfToken.php
+++ blog/app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,8 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         '/login',
         'posts',
-        'posts/*'
+        'posts/*',
+        'comments',
+        'comments/*'
     ];
 }
