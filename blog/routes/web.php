17318f4e623c9e4c6e9ac687dd79b01dd35a6c2e
kuninaa19 <minchan0527@naver.com>
2020-04-02 14:41:50 +0900

    (updated) web.php Article,Auth 디렉토리 생성에 따라 파일접근위치 변경

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,48 +2,48 @@
 
 use Illuminate\Support\Facades\Route;
 
-//인덱스
-Route::get('/', 'exampleController@index');
-// Ajax 연습페이지
-Route::get('/example', 'exampleController@example');
-Route::post('/idstore', 'exampleController@store');
+//구글 로그인 하기
+Route::get('login/google', 'Auth\LoginController@redirectToProvider');
+Route::get('login/google/callback', 'Auth\LoginController@handleProviderCallback');
+Route::get('oauth', function () {
+    return view('index')->with('greeting');
+});
 
-// 관리자 로그인
+//관리자로그인 관리자 아이디 생성
 Route::middleware(['cors'])->group(function(){
     Route::get('/csrf_token', function(){
         return csrf_token();
     });
-    Route::post('login','AdminController@login');
-    Route::post('make_id','AdminController@storeSecret');
+    Route::post('make_id','Auth\AdminController@storeSecret');
+    Route::post('login','Auth\AdminController@login');
 });
 
 
 Route::middleware(['cors'])->group(function(){
     //글 관련 Route
-    Route::resource('posts', 'PostsController',['except' => ['index','create']]);
+    Route::resource('posts', 'Article\PostsController',['except' => ['index','create']]);
 
     //글작성 이미지저장
-    Route::post('posts/image', 'PostsController@imageStore');
+    Route::post('posts/image', 'Article\PostsController@imageStore');
 
     //카테고리별 글리스트
-    Route::get('posts/{category}/{num}', 'PostsController@category_list');
+    Route::get('posts/{category}/{num}', 'Article\PostsController@category_list');
 
     //글 조회수 +1 증가(작업 아직안함)
-    Route::get('posts/{num}', 'PostsController@viewUp');
+    Route::get('posts/{num}', 'Article\PostsController@viewUp');
 });
 
 Route::middleware(['cors'])->group(function(){
     // 댓글 관련 Route
-    Route::resource('comments', 'CommentsController',['except' => ['index','create','show']]);
+    Route::resource('comments', 'Article\CommentsController',['except' => ['index','create','show']]);
 
-    Route::get('comments/{postNum}/{page}', 'CommentsController@comments_list');
+    Route::get('comments/{postNum}/{page}', 'Article\CommentsController@comments_list');
 
 });
 
 Route::middleware(['cors'])->group(function(){
     // 대댓글 관련 Route
-    Route::resource('reply', 'ReplyController',['except' => ['index','create']]);
+    Route::resource('reply', 'Article\ReplyController',['except' => ['index','create']]);
 
-    Route::get('reply/{category}/{num}', 'ReplyController@kae');
-
+    Route::get('reply/{category}/{num}', 'Article\ReplyController@kae');
 });
