57be9c7ec1781499011e7a3708e9517cd5ebe884
kuninaa19 <minchan0527@naver.com>
2020-04-02 11:39:13 +0900

    (updated) AdminController.php 관리자 아이디생성 메소드만듦, 비빌번호 암호화해서 저장 및 로그인확인

--- blog/app/Http/Controllers/AdminController.php
+++ blog/app/Http/Controllers/AdminController.php
@@ -1,36 +1,58 @@
 <?php
+// 1. storeSecret() 관리자 아이디 생성
+// 2. login () 관리자 로그인인증
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Crypt;
 
 class AdminController extends Controller
 {
+    // 관리자 아이디 생성
+    public function storeSecret(Request $request)
+    {
+        $id = $request->input('id');
+        $pw = $request->input('pw');
+        $nickName = $request->input('nickName');
+
+        $encrypted = Crypt::encryptString($pw);
+
+        DB::table('admin')->insert(['id' => $id, 'pw'=> $encrypted,'nickName'=>$nickName]);
+    }
+
+    // 관리자 로그인인증
     public function login(Request $request)
     {
         $id = $request->input('id');
         $pw = $request->input('pw');
 
-        $admin = DB::table('admin')->where([['id', $id], ['pw', $pw]])->get();
+        $admin = DB::table('admin')->where('id', $id)->get();
 
-        //DB검색해서 가져온 값이 비었는지 확인
+        //DB검색해서 아이디 있는지 확인
         if (empty($admin[0]->id)) {
             $data = array(
                 'key' => false
             );
-        } else {
-            $data = array(
-                'key' => true,
-//                'user'=> array (
-//                        'firstName' => 'Brett',
-//                        'lastName' => 'McLaughlin',
-//                        'email' => 'brett@newInstance.com',
-//                    )
-            );
+            return json_encode($data, JSON_UNESCAPED_UNICODE);
         }
-        return json_encode($data, JSON_UNESCAPED_UNICODE);
+        else{
+            $decrypted = Crypt::decryptString($admin[0]->pw);
+
+            if($pw === $decrypted){
+                $data = array(
+                    'key' => true
+                );
+                return json_encode($data, JSON_UNESCAPED_UNICODE);
+            }
+            else{
+                $data = array(
+                    'key' => false
+                );
+                return json_encode($data, JSON_UNESCAPED_UNICODE);
+            }
+        }
 //      return response()->json($data);
-//      return response()->json([$value]);
     }
 }
