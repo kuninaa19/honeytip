fdecc02691a7ce0b6c158354c1904b5ec22fb28c
kuninaa19 <minchan0527@naver.com>
2020-04-07 18:38:28 +0900

    (updated) LoginController.php 1. redirectToProvider()에서 리프레시 토큰을 가져오기위해 액세스타입 오프라인으로 설정 2. handleProviderCallback() stateless 연결, 가입된 회원인지 확인하고 가입되어있으면 이름과 프로필수정 , 가입안되어있으면 새로 등록

--- blog/app/Http/Controllers/Auth/LoginController.php
+++ blog/app/Http/Controllers/Auth/LoginController.php
@@ -1,45 +1,63 @@
 <?php
+// 1. redirectToProvider() 구글에 로그인요청
+// 2. handleProviderCallback () 로그인한 후에 이미 만들어진 아이디인지 확인후 처리
 
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Socialite;
+use Auth;
+use App\User;
 
 class LoginController extends Controller
 {
 
+    // 1. redirectToProvider() 구글에 로그인요청
     public function redirectToProvider()
     {
-        return Socialite::driver('google')->redirect();
+//        return Socialite::driver('google')->redirect();
+        return Socialite::driver('google')->with(['access_type'=>'offline'])->redirect();
     }
 
+// 2. handleProviderCallback () 로그인한 후에 이미 만들어진 아이디인지 확인후 처리
     public function handleProviderCallback()
     {
-        $user = Socialite::driver('google')->user();
+        //아이디 생성 그런느낌인거같음.
+        $user = Socialite::driver('google')->stateless()->user();
+//        $user = Socialite::driver('google')->user();
 
-        $user->token;
+//        dd($user);
+        $existUser = User::where('email',$user->email)->first();
+        if($existUser){
+            if($user->refreshToken===null){
+                 User::where('email', $existUser->email)
+                    ->update(['name' => $user->getName()],['avatar' =>$user->getAvatar()]);
+            }
+            else{
+                User::where('email', $existUser->email)
+                    ->update(['name' => $user->getName()],['avatar' =>$user->getAvatar()],['refresh_token'=> $user->refreshToken]);
+            }
+//            $compareUser->save();
+            // 그전꺼로 로그인 되어있는 정보로 로그인해야함
+            auth()->login($existUser, false);
+        }
+        else{
+            $user = User::firstOrCreate([
+                'name'  => $user->getName(),
+                'email' => $user->getEmail(),
+                'avatar' =>$user->getAvatar(),
+                'sns_type'=>'google',
+                'refresh_token'=> $user->refreshToken
+//            'token'=>$user->token,
+            ]);
+            auth()->login($user, false);
+        }
 
-        // OAuth Two Providers
-        $token = $user->token;
-        $refreshToken = $user->refreshToken; // not always provided
-        $expiresIn = $user->expiresIn;
+//        dd($user);
 
-// OAuth One Providers
-        $token = $user->token;
-        $tokenSecret = $user->tokenSecret;
-
-// All Providers
-        $user->getId();
-        $user->getNickname();
-        $user->getName();
-        $user->getEmail();
-        $user->getAvatar();
+//        Auth::guard('admin')->login($user);
 
 
-        $user = Socialite::driver('google')->userFromToken($token);
-
-//        return Socialite::driver('google')->stateless()->user();
-
-
+        return redirect()->to('/');
     }
 }
