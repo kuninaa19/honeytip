526b971c8c9bdf4aef50d4bdbe7b2152f767e1a4
kuninaa19 <minchan0527@naver.com>
2020-04-07 18:46:45 +0900

    (added) web.php 구글로그인 추가(미완)

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,12 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
-//구글 로그인 하기
-Route::get('login/google', 'Auth\LoginController@redirectToProvider');
-Route::get('login/google/callback', 'Auth\LoginController@handleProviderCallback');
-Route::get('oauth', function () {
-    return view('index')->with('greeting');
+//Route::get('api/user', function () {
+//    // Only authenticated users may enter...
+//})->middleware('auth.basic');
+
+//메인페이지
+//auth미들웨어를 사용해야 로그인
+Route::middleware(['auth'])->group(function() {
+
+    Route::get('/', 'Controller@index');
 });
+//소셜로그아웃기능
+Route::get('/hi', 'exampleController@store');
+
+//소셜 로그인
+Route::get('/login', function () {
+    return view('/auth/login');
+})->name('login');
+
+Route::get('auth/social/google', 'Auth\LoginController@redirectToProvider');
+Route::get('/google/callback', 'Auth\LoginController@handleProviderCallback');
+
 
 //관리자로그인 관리자 아이디 생성
 Route::middleware(['cors'])->group(function(){
@@ -21,7 +36,7 @@ Route::middleware(['cors'])->group(function(){
 
 Route::middleware(['cors'])->group(function(){
     //글 관련 Route
-    Route::resource('posts', 'Article\PostsController',['except' => ['index','create']]);
+    Route::resource('posts', 'Article\PostsController',['except' => ['create']]);
 
     //글작성 이미지저장
     Route::post('posts/image', 'Article\PostsController@imageStore');
@@ -41,7 +56,7 @@ Route::middleware(['cors'])->group(function(){
 
 });
 
-Route::middleware(['cors'])->group(function(){
+Route::middleware(['cors','auth'])->group(function(){
     // 대댓글 관련 Route
     Route::resource('reply', 'Article\ReplyController',['except' => ['index','create']]);
 
