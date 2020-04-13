a95e06496c787c4ccf25cc0694927264ba22007b
kuninaa19 <minchan0527@naver.com>
2020-04-13 11:24:36 +0900

    (updated) ReplyController.php 대댓글 추가,작성내용불러오기,수정,삭제 작동되도록 수정

--- blog/app/Http/Controllers/Article/ReplyController.php
+++ blog/app/Http/Controllers/Article/ReplyController.php
@@ -19,21 +19,24 @@ class ReplyController extends Controller
 
     }
 
-    //댓글 DB저장하기
+    //대댓글 DB저장하기
     public function store(Request $request)
     {
         $name = $request->input('userName');
-        $comment = $request->input('comment');
-        $category = $request->input('category');
+        $reply = $request->input('reply');
+        $parent = $request->input('indexComment');
         $postNum = $request->input('postNum');
 
-        $store = DB::table('comments')->insertGetId(['userName' => $name, 'comment' => $comment,
-            'postNum' => $postNum, 'category'=> $category, 'date'=>NOW()]);
+        $orderNum = DB::table('reply')->where(['postNum'=>$postNum,'parentComments'=>$parent])->orderBy('order','asc')->first();
 
-        $confirm = DB::table('comments')->where('indexComments',$store)->first();
+
+        $store = DB::table('reply')->insertGetId(['userName' => $name, 'reply' => $reply,
+            'postNum' => $postNum, 'parentComment'=> $parent, 'date'=>NOW(),'order'=>$orderNum+1]);
+
+        $confirm = DB::table('reply')->where('indexRely',$store)->first();
 
         //DB검색해서 가져온 값이 비었는지 확인
-        if(empty($confirm->indexComments)){
+        if(empty($confirm->indexReply)){
             $data = array(
                 'key'=>false
             );
@@ -41,18 +44,18 @@ class ReplyController extends Controller
         else {
             $data = array(
                 'key'=>true,
-                'commentIndex'=>$store
+                'replyIndex'=>$confirm
             );
         }
         return json_encode($data,JSON_UNESCAPED_UNICODE);
     }
 
-    // 댓글 수정하기위한 작성된 댓글 내용가져오기
+    // 대댓글 수정하기위한 작성된 댓글 내용가져오기
     public function edit($id)
     {
-        $comment = DB::table('comments')->where('indexComments', $id)->first();
+        $reply = DB::table('reply')->where('indexReply', $id)->first();
 
-        if(empty($comment->indexComments)){
+        if(empty($reply->indexReply)){
             $data = array(
                 'key'=>false
             );
@@ -60,18 +63,18 @@ class ReplyController extends Controller
         else{
             $data = array(
                 'key'=>true,
-                'comment' => $comment->comment
+                'comment' => $reply->reply
             );
         }
         return json_encode($data,JSON_UNESCAPED_UNICODE);
     }
 
-    // 댓글 수정하기 버튼 누르고 DB에 수정된 댓글 저장
+    // 대댓글 수정하기 버튼 누르고 DB에 수정된 댓글 저장
     public function update(Request $request, $id)
     {
-        $comment = $request->input('comment');
+        $reply = $request->input('reply');
 
-        DB::table('comments')->where('indexComments', $id)->update(['comment'=>$comment]);
+        DB::table('reply')->where('indexReply', $id)->update(['reply'=>$reply]);
 
         $data = array(
             'key'=>true
@@ -79,20 +82,20 @@ class ReplyController extends Controller
         return json_encode($data,JSON_UNESCAPED_UNICODE);
     }
 
-    // 댓글 삭제
+    // 대댓글 삭제
     public function destroy($id)
     {
-        $confirm = DB::table('comments')->where('indexComments',$id)->first();
+        $confirm = DB::table('reply')->where('indexReply',$id)->first();
 
         //DB검색해서 가져온 값이 비었다. 이미 삭제됨
-        if(empty($confirm->indexComments)){
+        if(empty($confirm->indexReply)){
             $data = array(
                 'key'=>false
             );
             return json_encode($data,JSON_UNESCAPED_UNICODE);
         }
 
-        DB::table('comments')->where('indexComments', $id)->delete();
+        DB::table('reply')->where('indexReply', $id)->delete();
 
         $data = array(
             'key'=>true
