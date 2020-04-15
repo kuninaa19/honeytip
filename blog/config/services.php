6dc2bfd8c306f202484459db5ac20e6168a689fe
kuninaa19 <minchan0527@naver.com>
2020-04-15 12:57:20 +0900

    (added) services.php 깃허브, 네이버, 페이스북 로그인 연동코드 작성

--- blog/config/services.php
+++ blog/config/services.php
@@ -33,5 +33,20 @@ return [
         'client_id' => env('GOOGLE_CLIENT_ID'),         // Your GOOGLE Client ID
         'client_secret' => env('GOOGLE_CLIENT_SECRET'), // Your GOOGLE Client Secret
         'redirect' => env('GOOGLE_REDIRECT'),
+    ],
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => env('FACEBOOK_REDIRECT')
+    ],
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => env('GITHUB_REDIRECT')
+    ],
+    'naver' => [
+        'client_id' => env('NAVER_CLIENT_ID'),
+        'client_secret' => env('NAVER_CLIENT_SECRET'),
+        'redirect' => env('NAVER_REDIRECT')
     ]
 ];
